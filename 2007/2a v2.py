5885c1ec36b6684ecaeb1bcd7720157fae8c0faf
RetroCoder13 <archie.anthony@blueyonder.co.uk>
2023-03-12 16:00:19 +0000

    Started work on Q2

--- 2007/2a v2.py
+++ 2007/2a v2.py
@@ -1,5 +1,8 @@
-startingPos = "EOOOOXXXX"
-possibleMoves = []
+# Example win for X: XXOOOOXXE
+#                     ^^^^^^
+
+# currentPos = "EOOOOXXXX"
+currentPos = "OXOEOXXOX"
 
 def stringToList(string):
     list = []
@@ -16,6 +19,7 @@ def listToString(list):
 def possibleMoves(position,letter):
     firstLetterPos = position.find(letter)
     emptyPos = position.find("E")
+    moves = []
     for i in range(4):
         listOfPos = stringToList(position)
         if(i == 0):
@@ -26,9 +30,43 @@ def possibleMoves(position,letter):
         listOfPos[emptyPos] = letter
         listOfPos[nextLetterPos] = "E"
 
-        print(listToString(listOfPos))
+        moves += [listToString(listOfPos)]
+
+    return moves
         
         # newPosition = position.replace(position[emptyPos],position[nextLetterPos])
         # print(newPosition)
 
-possibleMoves(startingPos, "X")
+def stratOne(moves,letter):
+    for move in moves:
+        if letter == "X":
+            if "XEX" in move[1:] and move[0] == letter:
+                return move
+        if letter == "O":
+            if "OEO" in move[1:] and move[0] == letter:
+                return move
+
+def stratTwo(moves,opponentLetter):
+    for move in moves:
+        possibleOpponentMoves = possibleMoves(move, opponentLetter)
+        if not stratOne(possibleOpponentMoves, opponentLetter):
+            return move
+    return moves[0]
+
+def checkWin(position):
+        if "XEX" in position[1:] and position[0] == "X":
+            return position
+        if "OEO" in position[1:] and position[0] == "O":
+            return position
+
+playerXMoves = possibleMoves(currentPos, "X")
+if stratOne(playerXMoves, "X") == None:
+    currentPos = stratTwo(playerXMoves, "O")
+else:
+    print("YOU WIN")
+
+playerOMoves = possibleMoves(currentPos, "O")
+if stratOne(playerXMoves, "O") == None:
+    currentPos = stratTwo(playerXMoves, "X")
+else:
+    print("OPPONENT WIN")
